f29654e4e2a8a0098999e77502e806ce6b98ba34
Bernardo Freitas <125708670+bernardhito@users.noreply.github.com>
2024-03-08 14:44:06 -0300

    Add files via upload

---
+++ Exercicio/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$nome  = "Bernardo";
+$sobrenome = "Freitas";
+$celular = "11 96735-2649";
+$email = "befreitas00@gmail.com";
+
+echo "Seu nome é : $nome <br>";
+
+echo "Seu sobrenome é: $sobrenome <br>";
+
+echo "Seu número de celular é: $celular <br>";
+
+echo "Seu email é: $email <br>";
+
+?>
