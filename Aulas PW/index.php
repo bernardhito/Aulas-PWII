996615b6e5f53b5634463a8c2e00b2d8ecf0a406
Bernardo Freitas <125708670+bernardhito@users.noreply.github.com>
2024-03-08 14:44:28 -0300

    Add files via upload

---
+++ Aulas PW/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome      = "Bernardo";
+$email     =  90;
+$celular    =  true;
+$    =  null;
+
+
+echo  "Ola  ".$nome." ".$sobrenome." Voce tem " .$idade ." anos de idade <br><br>";
+echo  "Ola $nome $sobrenome Voce tem $idade anos de idade <br><br>";
+echo  "Ola $nome $sobrenome Voce tem $idade anos de idade";;
